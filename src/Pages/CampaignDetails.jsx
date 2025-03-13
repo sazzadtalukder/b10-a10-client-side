c8eed9e19871dbb9bcd10c30fec357992c41a3e4
sazzadtalukder <sazzadtalukdercseiu@gmail.com>
2025-03-13 21:20:54 +0600

    details page updated

--- src/Pages/CampaignDetails.jsx
+++ src/Pages/CampaignDetails.jsx
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2';
 import Loading from '../Components/Loading';
 
 const CampaignDetails = () => {
-    const {user,loading,setLoading} = useContext(AuthContext)
+    const { user, loading, setLoading } = useContext(AuthContext)
     const { id } = useParams();
     const [campaign, setCampaign] = useState([])
     useEffect(() => {
@@ -25,21 +25,21 @@ const CampaignDetails = () => {
     const campaignTitle = detailsData?.campaignTitle
     const description = detailsData?.description
     const minimumDonationAmount = detailsData?.minimumDonationAmount
-     const deadline  = detailsData?.deadline
+    const deadline = detailsData?.deadline
     const name = user?.displayName;
     const email = user?.email
-    const donatatedUser = {option,imageUrl,campaignTitle,description,minimumDonationAmount,deadline,name,email}
+    const donatatedUser = { option, imageUrl, campaignTitle, description, minimumDonationAmount, deadline, name, email }
     console.log(donatatedUser)
-    const handleDonation =()=>{
+    const handleDonation = () => {
         const todayDate = new Date().toISOString().split('T')[0]
-        if(detailsData.deadline < todayDate){
-            console.log(detailsData.deadline,todayDate)
+        if (detailsData.deadline < todayDate) {
+            console.log(detailsData.deadline, todayDate)
             console.log('it expired');
             Swal.fire({
                 title: "No!",
                 text: "Deadline is over",
                 icon: "error"
-              })
+            })
             return
         }
         fetch('http://localhost:5000/addDonation', {
@@ -49,37 +49,38 @@ const CampaignDetails = () => {
             },
             body: JSON.stringify(donatatedUser)
         })
-        .then(res => res.json())
-        .then(data => {
-             console.log('getting data',data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('getting data', data)
+            })
     }
-    if(loading){
+    if (loading) {
         return <Loading></Loading>
     }
     return (
         <div>
             {
-                detailsData && 
+                detailsData &&
                 <div className="hero bg-base-200 min-h-screen">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img
-                        src={detailsData?.imageUrl}
-                        className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">{detailsData.campaignTitle}</h1>
-                        <p className="py-6">
-                           {detailsData.description}
-                        </p>
-                       
-                            <p>Campaign Type: <span className='bg-green-400 rounded text-white font-bold'>{detailsData.option}</span></p>
-                            <p>Minimum Donation Amount: <span>{detailsData.minimumDonationAmount}$</span></p>
-                        
-                        <button className="btn btn-primary" onClick={handleDonation}>Donate</button> 
-                        <Link to='/myDonation'><button className="btn btn-primary" >My All Donation</button></Link>
+                    <div className="hero-content flex-col lg:flex-row">
+                        <img
+                            src={detailsData?.imageUrl}
+                            className="max-w-sm rounded-lg shadow-2xl" />
+                        <div className='text-start'>
+                            <h1 className="text-5xl font-bold">{detailsData.campaignTitle}</h1>
+                            <p className="py-6">
+                                {detailsData.description}
+                            </p>
+                            <p><span className="font-semibold">Category:</span> <span className="px-1  bg-green-400 rounded">{detailsData.option}</span></p>
+                                        <p><span className="font-semibold">Deadline:</span> <span className="px-1  bg-red-400 rounded">{detailsData.deadline}</span></p>
+                                        <p> </p>
+                                        <p className="text-left"><span className="font-semibold">Minimum Donation:</span> USD $<span className="px-1 bg-amber-400 rounded">{detailsData.minimumDonationAmount}</span></p>
+
+                            <button className="btn btn-primary" onClick={handleDonation}>Donate</button>  <br /> <br />
+                            <Link to='/myDonation'><button className="btn btn-primary" >My All Donation</button></Link>
+                        </div>
                     </div>
                 </div>
-            </div>
             }
         </div>
     );
